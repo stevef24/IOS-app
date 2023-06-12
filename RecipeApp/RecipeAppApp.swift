deaa256f80010a2a1f74300b1a93072f1e46e6ab
Stav Fernandes <stevefernandes24@hotmail.co.uk>
2023-06-12 16:46:42 +0100

    Initial Commit

---
+++ RecipeApp/RecipeAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  RecipeAppApp.swift
+//  RecipeApp
+//
+//  Created by Stav Fernandes on 12/06/2023.
+//
+
+import SwiftUI
+
+@main
+struct RecipeAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
