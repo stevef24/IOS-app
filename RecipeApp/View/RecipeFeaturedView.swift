1204466092832474711bd672b11efe23502ca9c9
Stav Fernandes <stevefernandes24@hotmail.co.uk>
2023-06-14 13:35:31 +0100

    added recipe feature view

---
+++ RecipeApp/View/RecipeFeaturedView.swift
@@ -0,0 +1,24 @@
+//
+//  RecipeFeaturedView.swift
+//  RecipeApp
+//
+//  Created by Stav Fernandes on 14/06/2023.
+//
+
+import SwiftUI
+
+
+struct RecipeFeaturedView: View {
+    
+    @EnvironmentObject var model:RecipeModel
+
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct RecipeFeaturedView_Previews: PreviewProvider {
+    static var previews: some View {
+        RecipeFeaturedView()
+    }
+}
--- RecipeApp/View/RecipeListView.swift
+++ RecipeApp/View/RecipeListView.swift
@@ -8,9 +8,8 @@
 import SwiftUI
 
 struct RecipeListView: View {
-    
-  @ObservedObject  var model = RecipeModel()
-    
+    @EnvironmentObject var model:RecipeModel
+
     var body: some View {
         
         
